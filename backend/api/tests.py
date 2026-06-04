11ddb733af6ca00b044b918816e9f567e33f3d0b
Ilya Dyachin <main@official-dyachin.ru>
2026-06-04 18:00:16 +0700

    test2

---
+++ backend/api/tests.py
@@ -0,0 +1,55 @@
+from http import HTTPStatus
+
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+
+from recipes.models import Tag, Recipe
+
+User = get_user_model()
+
+
+class FoodgramAPITestCase(TestCase):
+    def setUp(self):
+        self.guest_client = Client()
+
+        self.user = User.objects.create_user(
+            username='test_chef',
+            email='chef@foodgram.com',
+            password='Password123'
+        )
+
+        self.tag = Tag.objects.create(
+            name='Завтрак',
+            slug='breakfast',
+            color='#FFFFFF'
+        )
+
+        self.recipe = Recipe.objects.create(
+            author=self.user,
+            name='Тестовая яичница',
+            text='Просто пожарьте яйца',
+            cooking_time=5
+        )
+        self.recipe.tags.add(self.tag)
+
+    def test_recipes_list_exists(self):
+        """Проверка доступности списка рецептов для любого гостя."""
+        response = self.guest_client.get('/api/recipes/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_tags_list_exists(self):
+        """Проверка доступности списка тегов для гостя."""
+        response = self.guest_client.get('/api/tags/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_anonymous_cannot_create_recipe(self):
+        """Проверка, что анонимный юзер получает отказ при создании рецепта."""
+        data = {
+            'ingredients': [{'id': 1, 'amount': 10}],
+            'tags': [self.tag.id],
+            'name': 'Запрещенный рецепт',
+            'text': 'Аноним не должен это создать',
+            'cooking_time': 10
+        }
+        response = self.guest_client.post('/api/recipes/', data=data, content_type='application/json')
+        self.assertEqual(response.status_code, HTTPStatus.UNAUTHORIZED)
--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -62,9 +62,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'foodgram'),
         'USER': os.getenv('POSTGRES_USER', 'foodgram_user'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'mysecretpassword'),
-        'HOST': os.getenv('DB_HOST', 'db'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432),
     }
 }
 

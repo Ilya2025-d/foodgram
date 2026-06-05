efba7a22727bb78597714bee51f762d53f5a3ccf
Ilya Dyachin <main@official-dyachin.ru>
2026-06-05 18:56:52 +0700

    add csrf_trusted

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -117,3 +117,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 6,
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://ilyahost.sytes.net',
+    'https://*.sytes.net',
+]
